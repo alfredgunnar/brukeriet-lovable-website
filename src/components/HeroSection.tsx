aa9149a970b0bb8ae9fe996cd19ddf8300378573
Alfred Gunnargård <alfred.gunnargard@gmail.com>
2025-03-13 21:55:40 +0100

    fix: change hero image url

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
         ref={containerRef}
         className="absolute inset-0 w-full h-full"
         style={{
-          backgroundImage: 'url(/lovable-uploads/659630e8-7b75-45eb-9af5-cddb0b4b3f16.png)',
+          backgroundImage: 'url(https://storage.googleapis.com/gpt-engineer-file-uploads/q5weDLyZRpOVVC6Qv4QCV2OH98Z2/659630e8-7b75-45eb-9af5-cddb0b4b3f16?Expires=1741904019&GoogleAccessId=admin-2%40gpt-engineer-390607.iam.gserviceaccount.com&Signature=xQ7%2Fo%2BiZoO%2BeBko3TThAtqb6s%2FUtLc68k07FJacZP%2F7QlYTdffVS3gnZ5xzXtqVb%2FFHfdigx7bEnYZRtDPoVSRoBDh0x6kyzIrLm19aSM38sESK1op32jzc%2FtDPb5bX7xkKQ4zMfS8O%2BdeUK4SgScBJ1P5GM%2BqgP91UwbjV0uY3g9kEJQhQVqcW0FVSKQTrZTkT5m0BK8qXqx77eo99y2yc3brFu%2Fbnc8h%2BSdKXiNMRnE9O7%2FARTCzQ7ALU4mJgB3Sy3rSdCIUFZicsGqeJlDi8gBHN4GABSHqjD48T7yZhUu6JsNN0dJQYsmoN9BKtzrzS1XyppN4V6Pv4YPA%2F4Sw%3D%3D)',
           backgroundPosition: 'center',
           backgroundSize: 'cover',
           transform: 'translateZ(0)',
